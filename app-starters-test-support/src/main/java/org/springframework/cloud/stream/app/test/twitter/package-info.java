1159a0b781ab7d8f02e148444025b7e2b3bc8fea
Zed <zed.luo@acxiom.com>
2019-09-18 18:05:59 +0800

    testing

--- app-starters-test-support/src/main/java/org/springframework/cloud/stream/app/test/twitter/package-info.java
+++
@@ -1,4 +0,0 @@
-/**
- * Provides classes for testing twitter apps.
- */
-package org.springframework.cloud.stream.app.test.twitter;
--- generateApps.bat
+++
@@ -1,8 +0,0 @@
-echo off
-
-REM Changing Directory to project DIR
-cd /d %~dp0
-
-REM execute mvn plugin with app generation
-echo on
-mvnw clean install scs:generate-app -pl :spring-cloud-stream-app-generator
