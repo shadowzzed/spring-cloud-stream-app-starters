07e78e7acd7924b547d91867d8002e41e40a0451
Zed <zed.luo@acxiom.com>
2019-09-18 18:11:43 +0800

    testing

---
+++ test-sftp-spring-boot/src/main/java/com/zed/testsftpspringboot/TestSftpSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.zed.testsftpspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestSftpSpringBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestSftpSpringBootApplication.class, args);
+    }
+
+}
